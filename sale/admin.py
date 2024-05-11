40fdfdccbf4f64b3521aae1ef5ce15a5b05f0904
Akmaljon701 <akmalkhanever@gmail.com>
2024-05-11 22:04:03 +0500

    updated

--- sale/admin.py
+++ sale/admin.py
@@ -5,7 +5,7 @@ from sale.models import Sale
 class SaleAdmin(admin.ModelAdmin):
     list_display = ('id', 'product', 'client', 'sold_price', 'date')
     list_display_links = ('product', 'client', 'sold_price', 'date')
-    search_fields = ('product', 'client', 'sold_price', 'date')
+    search_fields = ('product__name', 'client__FIO', 'client__phone_number', 'sold_price', 'date')
     list_filter = ['date']
 
 
